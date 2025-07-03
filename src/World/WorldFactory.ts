4a5ba16845893b67aaa19e2fd7c395992946822c
damiencortina <git@damiencortina.fr>
2025-07-03 22:11:52 +0000

    ⚗️ Try another way to load chunks when moving (based on a movement vector)

--- src/World/WorldFactory.ts
+++ src/World/WorldFactory.ts
@@ -6,46 +6,98 @@ import { Utils } from "../Utils";
 import { Config } from "../Config";
 import { Character } from "../Character";
 
-type ChunkDictionnary = Record<string, Chunk>;
 export abstract class WorldFactory implements Observer {
     scene: Scene;
-    displayedChunks: ChunkDictionnary = {};
+    displayedChunks: Chunk[][] = [];
+    currentCoordinates: Vector3 | undefined;
 
     constructor(scene: Scene) {
         this.scene = scene;
         new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        //this.displayedChunks = Array(Config.distanceView * 2 + 1);
     }
 
     abstract createChunk(coordinates: Vector3): Chunk;
 
     generateworld(chunkCoordinates: Vector3) {
-        const newChunks: ChunkDictionnary = {};
-        Utils.range(
-            chunkCoordinates.x - Config.distanceView,
-            chunkCoordinates.x + Config.distanceView
-        ).forEach((x) => {
+        if (!this.currentCoordinates) {
+            const xRange = Utils.range(
+                chunkCoordinates.x - Config.distanceView,
+                chunkCoordinates.x + Config.distanceView
+            );
+            //const worldSize = xRange.length;
+            xRange.forEach((x) => {
+                // const chunkLine: Chunk[] = Array(worldSize);
+                const chunkLine: Chunk[] = [];
+                Utils.range(
+                    chunkCoordinates.z - Config.distanceView,
+                    chunkCoordinates.z + Config.distanceView
+                ).forEach((z) => {
+                    const coordinates = new Vector3(x, 0, z);
+                    const chunk = this.createChunk(coordinates);
+                    chunk.render();
+                    chunkLine.push(chunk);
+                });
+                this.displayedChunks.push(chunkLine);
+            });
+            this.currentCoordinates = chunkCoordinates;
+            return;
+        }
+        const movementVector = chunkCoordinates.subtract(
+            this.currentCoordinates
+        );
+        if (movementVector.x) {
+            const chunkLine: Chunk[] = [];
             Utils.range(
                 chunkCoordinates.z - Config.distanceView,
                 chunkCoordinates.z + Config.distanceView
-            ).forEach((z) => {
-                const coordinates = new Vector3(x, 0, z);
-                const index = coordinates.toString();
-                if (!this.displayedChunks[index]) {
-                    const chunk = this.createChunk(coordinates);
-                    chunk.render();
-                    newChunks[index] = chunk;
-                } else {
-                    newChunks[index] = this.displayedChunks[index];
-                }
+            ).forEach((zCoordinate) => {
+                const chunk = this.createChunk(
+                    new Vector3(
+                        chunkCoordinates.x +
+                            movementVector.x * Config.distanceView,
+                        0,
+                        zCoordinate
+                    )
+                );
+                chunk.render();
+                chunkLine.push(chunk);
             });
-        });
-        Object.values(this.displayedChunks).forEach((chunk: Chunk) => {
-            if (!newChunks[chunk.coordinates.toString()]) {
-                chunk.remove();
-            }
-        });
+            this.displayedChunks
+                .splice((movementVector.x - 1) / 2, 1)[0]
+                .map((chunk: Chunk) => chunk.remove());
+            this.displayedChunks.splice(
+                ((-movementVector.x - 1) / -2) * (Config.distanceView * 2 + 1),
+                0,
+                chunkLine
+            );
+        }
 
-        this.displayedChunks = newChunks;
+        if (movementVector.z) {
+            this.displayedChunks = this.displayedChunks.map((chunkLine) => {
+                const chunk = this.createChunk(
+                    new Vector3(
+                        chunkLine[0].coordinates.x,
+                        0,
+                        chunkCoordinates.z +
+                            movementVector.z * Config.distanceView
+                    )
+                );
+                chunk.render();
+                const newChunkLine = chunkLine;
+                newChunkLine
+                    .splice((movementVector.z - 1) / 2, 1)
+                    .map((chunk: Chunk) => chunk.remove());
+                newChunkLine.splice(
+                    ((-movementVector.z - 1) / -2) *
+                        (Config.distanceView * 2 + 1),
+                    0,
+                    chunk
+                );
+                return newChunkLine;
+            });
+        }
+        this.currentCoordinates = chunkCoordinates;
     }
 
     update(subject: Subject): void {
